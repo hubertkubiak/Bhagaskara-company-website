ac0f28928f705f3290fa3d443f21f4ba1af875f2
hubertkubiak <hubkubiak@gmail.com>
2015-10-26 09:48:03 +0100

    adding about-us and interlude sections

--- js/js.js
+++ js/js.js
@@ -1,3 +1,48 @@
 /**
  * Created by Hubert on 2015-10-21.
  */
+$(function(){
+
+var Application = function(){
+       function init() {
+           console.log("init");
+       }
+       function scrollPage(){
+           console.log("scrolling");
+
+
+       }
+
+       return {
+           init: init ,
+           scrollPage: scrollPage,
+           kot: 2
+       };
+};
+
+
+var app = new Application();
+app.init();
+
+
+
+});
+
+/*
+
+$(function(){
+    $('#left').mouseover(
+        function(){ $(this).hide()}
+    );
+    $('#left').mouseleave(
+        function(){ $(this).show() }
+    );
+    $('#right').mouseover(
+        function(){ $(this).hide() }
+    );
+    $('#right').mouseleave(
+        function(){ $(this).show() }
+    )
+});
+
+*/
