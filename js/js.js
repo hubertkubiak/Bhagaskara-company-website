26ec2b7d2b15c509ea3025d4dc2d28afc1d07412
hubertkubiak <hubkubiak@gmail.com>
2015-10-26 12:52:11 +0100

    css and js update

--- js/js.js
+++ js/js.js
@@ -12,7 +12,6 @@ var Application = function(){
 
 
        }
-
        return {
            init: init ,
            scrollPage: scrollPage,
@@ -23,10 +22,63 @@ var Application = function(){
 
 var app = new Application();
 app.init();
-
-
-
 });
+
+
+
+
+$(function(){
+    var menu = $(".navbar");
+    var sections = $(".sec");
+    var links = $(".navbar .menu a").not(document.getElementById("notblog"));
+    var lastMenuPositionFromTop = 0;
+    var menuHeight = menu.height();
+
+    $(window).scroll(function(event){
+        if( menu.hasClass("sticky") === false &&
+            $(this).scrollTop() > menu.offset().top){
+            lastMenuPositionFromTop = menu.offset().top;
+            menu.addClass("sticky");
+        }
+        if( menu.hasClass("sticky") &&
+            $(this).scrollTop() < lastMenuPositionFromTop) {
+            menu.removeClass("sticky");
+        }
+
+        sections.each(function(index){
+            if(index + 1 >= sections.length){
+                if( sections.eq(index).offset().top < $(window).scrollTop()){
+                    links.eq(index).addClass("active");
+                }
+                else{
+                    links.eq(index).removeClass("active");
+                }
+            }
+            else{
+
+                if( sections.eq(index).offset().top < $(window).scrollTop() &&
+                    sections.eq(index + 1).offset().top > $(window).scrollTop()){
+
+                    links.eq(index).addClass("active");
+                }
+                else{
+                    links.eq(index).removeClass("active");
+                }
+            }
+
+
+        });
+
+    });
+});
+
+
+$('a[href*=#navbar]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
+
 
 /*
 
