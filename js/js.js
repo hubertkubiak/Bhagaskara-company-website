9708acda5cdf6fa1272358a625bc671e7fd99974
hubertkubiak <hubkubiak@gmail.com>
2015-10-27 16:30:23 +0100

    creatin the contact form and uploading social media buttons

--- js/js.js
+++ js/js.js
@@ -8,75 +8,87 @@ var Application = function(){
            console.log("init");
        }
        function scrollPage(){
-           console.log("scrolling");
-
-
+           var navigationOpt = $(".navigation a");
+           navigationOpt.on("click", function(event){
+               var hrefs = $(this).attr("href");
+               $('html,body').animate({
+                   scrollTop:$(hrefs).offset().top-40
+               }, 1000);
+           });
+           var menuOpt = $(".menu1 a");
+           menuOpt.on("click", function(event){
+               var hrefs = $(this).attr("href");
+               $('html,body').animate({
+                   scrollTop:$(hrefs).offset().top-2
+               }, 600);
+           });
+           var scroll = $(".scroll");
+           scroll.on("click", function(event){
+               $('html,body').animate({
+                   scrollTop:$(".navbar").offset().top-16
+               }, 600);
+           });
+           var toTop = $(".totopbottom");
+           toTop.on("click", function(event){
+               $('html,body').animate({
+                   scrollTop:$("#top").offset().top
+               }, 1000);
+           });
        }
+        function stickyMenu (){
+            var menu = $(".navbar");
+            var sections = $(".sec");
+            var links = $(".navbar .menu a").not(document.getElementById("notblog"));
+            var lastMenuPositionFromTop = 0;
+
+            $(window).scroll(function(event){
+                if( menu.hasClass("sticky") === false &&
+                    $(this).scrollTop() > menu.offset().top){
+                    lastMenuPositionFromTop = menu.offset().top;
+                    menu.addClass("sticky");
+                }
+                if( menu.hasClass("sticky") &&
+                    $(this).scrollTop() < lastMenuPositionFromTop) {
+                    menu.removeClass("sticky");
+                }
+                sections.each(function(index){
+                    if(index + 1 >= sections.length){
+                        if( sections.eq(index).offset().top < $(window).scrollTop()){
+                            links.eq(index).addClass("active");
+                        }
+                    else{
+                        links.eq(index).removeClass("active");
+                    }
+                    }
+                    else{
+                        if( sections.eq(index).offset().top < $(window).scrollTop() &&
+                            sections.eq(index + 1).offset().top > $(window).scrollTop()){
+                            links.eq(index).addClass("active");
+                        }
+                        else{
+                            links.eq(index).removeClass("active");
+                        }
+                    }
+                });
+
+            });
+        }
        return {
            init: init ,
            scrollPage: scrollPage,
-           kot: 2
+           stickyMenu: stickyMenu
        };
 };
 
 
 var app = new Application();
-app.init();
+    app.init();
+    app.scrollPage();
+    app.stickyMenu();
 });
 
 
 
-
-$(function(){
-    var menu = $(".navbar");
-    var sections = $(".sec");
-    var links = $(".navbar .menu a").not(document.getElementById("notblog"));
-    var lastMenuPositionFromTop = 0;
-    var menuHeight = menu.height();
-
-    $(window).scroll(function(event){
-        if( menu.hasClass("sticky") === false &&
-            $(this).scrollTop() > menu.offset().top){
-            lastMenuPositionFromTop = menu.offset().top;
-            menu.addClass("sticky");
-        }
-        if( menu.hasClass("sticky") &&
-            $(this).scrollTop() < lastMenuPositionFromTop) {
-            menu.removeClass("sticky");
-        }
-
-        sections.each(function(index){
-            if(index + 1 >= sections.length){
-                if( sections.eq(index).offset().top < $(window).scrollTop()){
-                    links.eq(index).addClass("active");
-                }
-                else{
-                    links.eq(index).removeClass("active");
-                }
-            }
-            else{
-
-                if( sections.eq(index).offset().top < $(window).scrollTop() &&
-                    sections.eq(index + 1).offset().top > $(window).scrollTop()){
-
-                    links.eq(index).addClass("active");
-                }
-                else{
-                    links.eq(index).removeClass("active");
-                }
-            }
-
-
-        });
-
-    });
-});
-
-
-$('a[href*=#navbar]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-});
 
 
 
